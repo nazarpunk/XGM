ee5d1d7863c515aef7156112cbd93e0bbe904ddc
nazarpunk <nazarpunk@gmail.com>
2019-04-27 15:56:46 +0300

    start

---
+++ paste-image.user.js
@@ -0,0 +1,14 @@
+'use strict';
+// ==UserScript==
+// @name XGM - Загрузка изображений из буфера
+// @description Добавляет возможность загружать изображения
+// @author nazarpunk
+// @license MIT
+// @version 1.0
+// @include /^https?:\/\/pb\.xgm\.guru.*$/
+// @grant unsafeWindow
+// @noframes
+// @run-at document-end
+// @source https://xgm.guru/user/NazarPunk
+// ==/UserScript==
+'use strict';
