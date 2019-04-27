34450757b675727b3046d89aba38745fb772ff78
nazarpunk <nazarpunk@gmail.com>
2019-04-27 23:26:38 +0300

    optimize

--- paste-image.user.js
+++ paste-image.user.js
@@ -1,55 +1,42 @@
 // ==UserScript==
 // @name XGM - Загрузка изображений из буфера
 // @description Добавляет возможность загружать изображения из буфер
-// @author nazarpunk
+// @author https://xgm.guru/user/NazarPunk
 // @license MIT
-// @version 1.0
+// @version 1.1
 // @include /^https?:\/\/xgm\.guru.*$/
 // @noframes
 // @run-at document-end
-// @source https://xgm.guru/user/NazarPunk
+// @source https://github.com/nazarpunk/XGM
 // ==/UserScript==
 'use strict';
-
-function FakeDataTransfer(file) {
-	this.dropEffect = 'all';
-	this.effectAllowed = 'all';
-	this.items = [];
-	this.types = ['Files'];
-	this.getData = function () {
-		return file;
-	};
-	this.files = [file];
-}
-
 document.addEventListener("DOMContentLoaded", () => {
 	const $textarea = document.getElementById('input_comment');
 	const $files = document.getElementById('id_files');
 	const $area = document.getElementsByClassName("qq-upload-drop-area");
 
 	if ($textarea === null || $area === null || $files === null || !window.Clipboard || !window.File) { return; }
-	window.addEventListener("paste", (e) => {
+	window.addEventListener("paste", e => {
 		if (!e.clipboardData) { return; }
-
-		const items = e.clipboardData.items;
-		for (let i = 0; i < items.length; i++) {
-			if (items[i].type.indexOf("image") !== -1) {
-				const event = new DragEvent('drop');
-				Object.defineProperty(
-					event,
-					'dataTransfer',
-					{
-						value: new FakeDataTransfer(
-							new File([items[i].getAsFile()],
-								`${Date.now()}.jpg`,
-								{type: "image/jpg"})
-						)
-					}
-				);
-
-				$files.style.display = `block`;
-				$area[0].dispatchEvent(event);
-			}
+		const files = [];
+		for (let i = 0; i < e.clipboardData.files.length; i++) {
+			let file = e.clipboardData.files[i];
+			files.push(new File([file], file.name.replace('image', Date.now()), {type: file.type}));
 		}
+		if (files.length === 0) { return; }
+
+		const event = new DragEvent('drop');
+		Object.defineProperty(
+			event,
+			'dataTransfer',
+			{
+				value: {
+					files: files
+				}
+			}
+		);
+
+		$files.style.display = `block`;
+		$area[0].dispatchEvent(event);
 	});
 });
