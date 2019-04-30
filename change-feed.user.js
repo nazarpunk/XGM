b8e3f4cde66c3e226674326bde42cc524a9fe013
nazarpunk <nazarpunk@gmail.com>
2019-04-30 09:14:54 +0300

    start

---
+++ change-feed.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name XGM - Переключение вкладок ленты
+// @description Улучшенное переключение вкладок ленты
+// @author https://xgm.guru/user/NazarPunk
+// @license MIT
+// @version 1.0
+// @include /^https?:\/\/xgm\.guru.*$/
+// @noframes
+// @run-at document-end
+// @source https://github.com/nazarpunk/XGM
+// ==/UserScript==
+'use strict';
+console.log('f', window.ChangeFeed);
